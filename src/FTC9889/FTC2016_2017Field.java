60c9c358116e349161c69f506776c8abc2e9065e
Unknown <heinrich.joshua@student.ccm.edu>
2017-07-20 01:28:55 -0400

    Reformated code, Added demo

--- src/FTC9889/FTC2016_2017Field.java
+++ src/FTC9889/FTC2016_2017Field.java
@@ -6,19 +6,23 @@ import usfirst.frc.team2168.robot.FalconLinePlot;
 
 public class FTC2016_2017Field {
 	
+	public static void main(String[] args) {
+		//Demonstrate FTC FieldMaker
+		FalconLinePlot FTCfield = new FalconLinePlot(new double[][]{{0.0,0.0}});
+		make(FTCfield);
+	}
+	
 	//Generate Field
 	public static void make(FalconLinePlot Plot){
 		
 		Plot.setBackground(Color.white);
 		Plot.setYLabel("Y (feet)");
 		Plot.setXLabel("X (feet)");
-		Plot.setTitle("Top Down View of FTC Field (12ft x 12ft) \n shows global position of robot path, along with left and right wheel trajectories");
+		Plot.setTitle("Top Down View of FTC Field (12ft x 12ft)");
 
+		//FTC General field
 		double FTCfieldWidth = 12.0;
-		Plot.setXTic(0, FTCfieldWidth, 1);
-		Plot.setYTic(0, FTCfieldWidth, 1);
-		
-		double[][] CenterLine = new double[][] {{0,0},{FTCfieldWidth, FTCfieldWidth}};
+		double[][] wall = new double[][] {{12,0},{12,12},{0,12},};
 		double[][] FieldTiles = new double[][] {
 			{1*(FTCfieldWidth/6), 0},{1*(FTCfieldWidth/6), FTCfieldWidth},
 			{2*(FTCfieldWidth/6), FTCfieldWidth},{2*(FTCfieldWidth/6), 0},
@@ -32,6 +36,9 @@ public class FTC2016_2017Field {
 			{0,4*(FTCfieldWidth/6)},{FTCfieldWidth, 4*(FTCfieldWidth/6)},{0,4*(FTCfieldWidth/6)},
 			{0,5*(FTCfieldWidth/6)},{FTCfieldWidth, 5*(FTCfieldWidth/6)},{0,5*(FTCfieldWidth/6)},
 		};
+		
+		//Velocity Vortex specific code
+		double[][] CenterLine = new double[][] {{0,0},{FTCfieldWidth, FTCfieldWidth}};
 		double[][] redCorner = new double[][] {{0,9}, {3,12},};
 		double[][] blueCorner = new double[][] {{9,0},{12,3},};
 		double[][] centerGoal = new double[][] {{5,5},{7,5},{7,7},{5,7},{5,5},};
@@ -42,10 +49,16 @@ public class FTC2016_2017Field {
 			{9,12},{9.3,12},{9.3,11.9},{8.7,11.9},{8.7,12},
 			{5,12},{5.3,12},{5.3,11.9},{4.7,11.9},{4.7,12},
 		};
-		double[][] wall = new double[][] {{12,0},{12,12},{0,12},};
 		
-		Plot.addData(CenterLine, Color.black);
+		/*
+		double[][] ball = new double[][] {
+			
+		};
+		*/
 
+		Plot.setXTic(0, FTCfieldWidth, 1);
+		Plot.setYTic(0, FTCfieldWidth, 1);	
+		Plot.addData(CenterLine, Color.black);
 		Plot.addData(redCorner, Color.RED);
 		Plot.addData(blueCorner, Color.blue);
 		Plot.addData(centerGoal, Color.BLACK);
